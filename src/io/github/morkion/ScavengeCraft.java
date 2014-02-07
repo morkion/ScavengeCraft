25062aa2ed938124d0560e997c20db2a0ed0f883
morkion <morkion@gmail.com>
2014-02-07 15:29:28 +0200

    Added random item drops

--- src/io/github/morkion/ScavengeCraft.java
+++ src/io/github/morkion/ScavengeCraft.java
@@ -1,5 +1,7 @@
 package io.github.morkion;
 
+import java.util.Random;
+
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
@@ -21,7 +23,38 @@ public class ScavengeCraft extends JavaPlugin implements Listener {
 		Block b = event.getBlock();
 		Material bm = b.getType();
 		if(bm == Material.DEAD_BUSH||bm == Material.CROPS||bm == Material.SUGAR_CANE_BLOCK||bm == Material.YELLOW_FLOWER||bm == Material.LONG_GRASS){
-			ItemStack item = new ItemStack(Material.ARROW);
+			ItemStack item = new ItemStack(Material.ROTTEN_FLESH);
+			int amount = new Random().nextInt(30);
+			short dmg = (short)new Random().nextInt(200);
+			switch(new Random().nextInt(20)){
+			case 0:
+				item = new ItemStack(Material.ARROW,amount);
+				break;
+			case 1:
+				item = new ItemStack(Material.APPLE, amount);
+				break;
+			case 2:
+				item = new ItemStack(Material.BOW,1,dmg);
+				break;
+			case 3:
+				item = new ItemStack(Material.CHAINMAIL_BOOTS,1,dmg);
+				break;
+			case 4:
+				item = new ItemStack(Material.CHAINMAIL_CHESTPLATE,1,dmg);
+				break;
+			case 5:
+				item = new ItemStack(Material.CHAINMAIL_HELMET,1,dmg);
+				break;
+			case 6:
+				item = new ItemStack(Material.CHAINMAIL_LEGGINGS,1,dmg);
+				break;
+			case 7:
+				item = new ItemStack(Material.STICK, amount);
+				break;
+			case 8:
+				item = new ItemStack(Material.IRON_INGOT, amount);
+				break;
+			}
 			b.getWorld().dropItem(b.getLocation(),item);
 		}
 	}
